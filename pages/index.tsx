de54ec7c8d3a3a8d02bc9f3e066d6de3d6faeeef
Mohamed <mohamedhersidev@gmail.com>
2025-01-04 09:00:48 -0500

    Added main page layout and component organization

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,21 @@ import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import HabitTracking from "../components/HabitTracking";
+import FeatureShowcase from "../components/FeatureShowcase";
+import WellnessJournal from "../components/WellnessJournal";
+import MeditationSection from "../components/MeditationSection";
+import HealthMetrics from "../components/HealthMetrics";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HealthMetrics />
       <HabitTracking />
+      <MeditationSection />
+      <WellnessJournal />
+      <FeatureShowcase />
     </div>
   );
 };
