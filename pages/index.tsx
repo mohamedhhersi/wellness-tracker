d1c1027710fc769cfaffca259a7bb374df05ece8
Mohamed <mohamedhersidev@gmail.com>
2025-01-12 05:07:24 -0500

    fixed importing issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import HabitTracking from "../components/HabitTracking";
