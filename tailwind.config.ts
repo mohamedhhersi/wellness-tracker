64f1c49ce1ba7c731d412e982aaee29fa46e0e63
Mohamed <mohamedhersidev@gmail.com>
2025-01-30 03:48:38 -0500

    fixed color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#4F46E5",
+          dark: "#4338CA",
+          light: "#6366F1",
+        },
+        secondary: {
+          DEFAULT: "#10B981",
+          dark: "#059669",
+          light: "#34D399",
+        },
+        "mice-elf": {
+          light: "#4A3F69",
+          dark: "#2A1B3D"
+        }
       },
     },
   },
